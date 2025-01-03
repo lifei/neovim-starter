8258456a6d85edc1f4c8a76f943eaf852fe04f2f
lifei <lifei.vip@outlook.com>
2025-01-03 07:51:12 +0000

    Fix keymap issue

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,12 +8,14 @@ vim.keymap.set({ "i", "s" }, "<C-L>", function()
   local ls = require("luasnip")
   ls.change_choice(1)
 end, { silent = true })
-vim.keymap.set({ "i", "c", "l" }, "<D-v>", "<C-R>+")
-vim.keymap.set({ "n" }, "<D-v>", '"+p')
-vim.keymap.set({ "v" }, "<D-c>", '"+y')
 
+vim.keymap.set({ "i", "c", "l" }, "<D-v>", "<C-R>+")
 vim.keymap.set({ "i", "c", "l" }, "<C-v>", "<C-R>+")
+
+vim.keymap.set({ "n" }, "<D-v>", '"+p')
 vim.keymap.set({ "n" }, "<M-v>", '"+p')
+
+vim.keymap.set({ "v" }, "<D-c>", '"+y')
 vim.keymap.set({ "v" }, "<C-c>", '"+y')
 
 vim.keymap.set({ "v" }, "<leader>r`", "di``<ESC>P", { desc = "Surround with `" })
